ae0a537fc93fe6ac5b7d3dbbc264a97075650091
DAM2Sergi <sergi.floroj.dam@alumnescostafreda.cat>
2023-02-10 19:00:28 +0100

    First controller

---
+++ Unity_P2/Assets/Scripts/MCcontroller.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MCcontroller : MonoBehaviour
+{
+    float speed = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 position= transform.position;
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        //float horizontal = Input.GetAxis("Horizontal");
+        //float vertical = Input.GetAxis("Vertical");
+
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+        transform.position = position;
+
+    }
+}
