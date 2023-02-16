7a6a39a0f1e4d3f28bd1c3b67e6e446efe5c375f
DAM2Sergi <sergi.floroj.dam@alumnescostafreda.cat>
2023-02-16 20:22:56 +0100

    Update Controller
    -Groundcheck

--- Assets/Scripts/MCcontroller.cs
+++ Assets/Scripts/MCcontroller.cs
@@ -11,7 +11,9 @@ public class MCcontroller : MonoBehaviour
     public float speed = 8.0f;
     public float jump = 18f;
 
-    private Rigidbody2D rbPlayer;
+    [SerializeField] private Rigidbody2D rbPlayer;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
 
     // Start is called before the first frame update
     void Start()
@@ -27,13 +29,19 @@ public class MCcontroller : MonoBehaviour
         horizontal = Input.GetAxisRaw("Horizontal");//La funcio getAxisRaw canvia el valor de moviment de 0 a -1 de manera directa no gradual
 
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGounded())
         {
             rbPlayer.velocity = new Vector2(rbPlayer.velocity.x, jump);
         }
 
 
     }
+
+    private bool IsGounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
 
     private void FixedUpdate()
     {
